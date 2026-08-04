eb9f7393b5e5151b334c3e3f2e1cb933abffa3d7
MONISH <mkha22647@gmail.com>
2026-08-04 16:04:27 +0530

    Time: 744 ms (31.77%), Space: 0B (100%) - LeetHub

---
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT
+    user_id,
+    CONCAT(
+        UPPER(LEFT(LOWER(name), 1)),
+        SUBSTRING(LOWER(name), 2)
+    ) AS name
+FROM Users
+ORDER BY user_id ASC
